05a36e61c83ef03bc671c7acbdc1f716ac667d14
lamilekan263 <lamilekan263@gmail.com>
2022-12-26 13:54:54 +0100

    fixed vercel set up

--- src/app.js
+++ src/app.js
@@ -1,13 +1,20 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const api = require('./routes/api');
 
 
 
 const app = express();
 
+
+app.use(cors());
 app.use(express.json());
 
+
+app.use('/', (req, res) => {
+    res.send('news-api');
+});
 app.use('/api/v1', api);
 
 
